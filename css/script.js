b8479a2d6056f5e296da6d28ba4403bb81a51f7d
lipika.manglaa <lipika.mangla.web@gmail.com>
2023-05-07 16:10:24 +1000

    create javascript file , work- if else statement, for loop ,arary

---
+++ css/script.js
@@ -0,0 +1,21 @@
+var topics="HTML,Css,JavaScript";
+console.log(topics);
+
+var a=prompt("Please enter topic name!");
+if(a=='HTML'){
+    console.log("you can get information from here")
+}else if(a=='Css'){
+    console.log("You can get informaton from here")
+}else if(a=='javaScript'){
+    console.log("You can get informaton from here")
+}else{
+    console.log("No more information , just only infomation 'HTML', 'Css','JavaScript'")
+}
+
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0])
+
+for( var x=0; x<shapes.length;x++){
+    console.log(shapes[x])
+}
